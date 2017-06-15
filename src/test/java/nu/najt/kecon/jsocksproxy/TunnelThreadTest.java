4c20bbc4f845dfe48107643fa07dd22396fe2067
Kenny Colliander Nordin <kenny@kendanware.com>
2017-06-15 20:21:09 +0200

    Updated deprecated Mockito annotation

--- src/test/java/nu/najt/kecon/jsocksproxy/TunnelThreadTest.java
+++ src/test/java/nu/najt/kecon/jsocksproxy/TunnelThreadTest.java
@@ -13,8 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.mockito.Mockito.*;
-import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TunnelThreadTest {
